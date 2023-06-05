6df844b80fbb0a16cd957ac529b6626f8c86ce19
aashikamakkena <95276912+aashikamakkena@users.noreply.github.com>
2023-06-05 22:30:03 +0530

    Added usercard2

---
+++ usercard2.js
@@ -0,0 +1,5 @@
+let abduloldinfo={
+    imgurl:"https://1.bp.blogspot.com/-z_ecZJVXa5c/Xx6d8fKX3vI/AAAAAAAAAjw/oA7eeZbLopEi2KUCbSfaHNTEnXcDJkn6QCLcBGAsYHQ/s1600/APJ%2BAbdul%2BKalam%2BPhoto.jpg",
+    name:"APJ Abdul Kalam",
+    Description:"Prseident of India"
+}
