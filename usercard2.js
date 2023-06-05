9a1c6656e1c521ad2cab76999ca6120c119dc913
aashikamakkena <95276912+aashikamakkena@users.noreply.github.com>
2023-06-05 22:48:04 +0530

    Added flipdata

--- usercard2.js
+++ usercard2.js
@@ -1,12 +1,12 @@
 let abduloldinfo = {
-    imgurl:"https://1.bp.blogspot.com/-z_ecZJVXa5c/Xx6d8fKX3vI/AAAAAAAAAjw/oA7eeZbLopEi2KUCbSfaHNTEnXcDJkn6QCLcBGAsYHQ/s1600/APJ%2BAbdul%2BKalam%2BPhoto.jpg",
-    name:"APJ Abdul Kalam",
-    Description:"President of India"
+    imgurl: "https://1.bp.blogspot.com/-z_ecZJVXa5c/Xx6d8fKX3vI/AAAAAAAAAjw/oA7eeZbLopEi2KUCbSfaHNTEnXcDJkn6QCLcBGAsYHQ/s1600/APJ%2BAbdul%2BKalam%2BPhoto.jpg",
+    name: "APJ Abdul Kalam",
+    Description: "President of India"
 }
 let abdulyounginfo = {
-    imgurl:"https://im.indiatimes.in/content/2015/Jul/1_1438014687_725x725.jpg",
-    name:"A.P.J Abdul Kalam",
-    Description:"Scientist"
+    imgurl: "https://im.indiatimes.in/content/2015/Jul/1_1438014687_725x725.jpg",
+    name: "A.P.J Abdul Kalam",
+    Description: "Scientist"
 }
 let isabdulold = true;
 let displayobject;
